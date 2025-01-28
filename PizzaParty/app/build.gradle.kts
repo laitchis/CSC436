2a4e8e91974f367afc44a035351c06fe5ef95ea9
laitchis <laitchis@calpoly.edu>
2025-01-27 18:48:14 -0800

    Finished Pizza Party

--- PizzaParty/app/build.gradle.kts
+++ PizzaParty/app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
