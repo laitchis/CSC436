3adb97d12ac737baa6bb162dab8182d74f1e97e3
Luke Aitchison <lukeaitchison@Lukes-Air.lan>
2025-02-24 23:26:19 -0800

    8.6

--- CountdownTimer_Starter/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ CountdownTimer_Starter/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -13,11 +13,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import androidx.work.workDataOf
 import com.zybooks.countdowntimer.ui.TimerScreen
 import com.zybooks.countdowntimer.ui.TimerViewModel
 import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
+   private val permissionRequestLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+         val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+         Log.i("MainActivity", message)
+      }
 
    private val timerViewModel = TimerViewModel()
 
@@ -34,13 +49,26 @@ class MainActivity : ComponentActivity() {
             }
          }
       }
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+         }
+      }
    }
    override fun onStop() {
       super.onStop()
 
       // Start TimerWorker if the timer is running
       if (timerViewModel.isRunning) {
-         startWorker(timerViewModel.remainingMillis)
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                  Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+               startWorker(timerViewModel.remainingMillis)
+            }
+         } else {
+            startWorker(timerViewModel.remainingMillis)
+         }
       }
    }
 
